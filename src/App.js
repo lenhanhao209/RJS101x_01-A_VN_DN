3c878cfce11b435d49e335ca1d616c5e1db58272
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-19 15:57:35 +0700

    Lab3_2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import "./App.css";
 import React, { Component } from "react";
+import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./Components/MenuComponent";
+import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
-      <div>
+      <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
