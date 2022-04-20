6b48162f5a0f7611e473db5a0fb0413ff77952d0
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-20 14:53:25 +0700

    Importing module

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React, { Component } from "react";
-import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./components/MenuComponent";
-import { DISHES } from "./shared/dishes";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
-    );
-  }
-}
-
-export default App;
+import StaffList from "./components/StaffListComponent.js";
+import { STAFFS } from "./shared/staffs";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
