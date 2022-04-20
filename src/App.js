b2a6b03fa696d1d2fd2f484d170745b47f8e02a2
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-20 17:15:10 +0700

    The second - Declaring App

--- src/App.js
+++ src/App.js
@@ -4,3 +4,26 @@ import StaffList from "./components/StaffListComponent.js";
 import { STAFFS } from "./shared/staffs";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList staffs={this.state.staffs} />
+      </div>
+    );
+  }
+}
+
+export default App;
