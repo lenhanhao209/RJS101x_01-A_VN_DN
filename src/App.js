c994670a70f8888596be07b26915610a7017fd95
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-21 09:57:03 +0700

    The second - Declaring App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import StaffList from "./components/StaffListComponent.js";
 import { STAFFS } from "./shared/staffs";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      staffs: STAFFS,
-    };
-  }
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-        <StaffList staffs={this.state.staffs} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [staffs, setStaffs] = useState(STAFFS);
+  return (
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+        </div>
+      </Navbar>
+      <StaffList staffs={staffs} />
+    </div>
+  );
+};
 
 export default App;
