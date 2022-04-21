22dec4f0220b88b5ec08ea7494d466e546cf789d
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-21 11:08:13 +0700

    The fiveth - Declaring Staff

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -10,4 +10,35 @@ const StaffList = (props) => {
   const columnSelect = (col) => {
     setColumDefault(col);
   };
+  const Staff = () => {
+    if (selectedStaff !== null) {
+      return (
+        <div className="col-4">
+          <Card>
+            <CardImg
+              width="100%"
+              src={selectedStaff.image}
+              alt={selectedStaff.name}
+            />
+            <CardBody>
+              <CardTitle>{`Họ và tên: ${selectedStaff.name}`}</CardTitle>
+              <CardText>{`Ngày sinh: ${dateFormat(
+                selectedStaff.doB,
+                "dd/mm/yyyy"
+              )}`}</CardText>
+              <CardText>{`Ngày vào công ty: ${dateFormat(
+                selectedStaff.startDate,
+                "dd/mm/yyyy"
+              )}`}</CardText>
+              <CardText>{`Phòng ban: ${selectedStaff.department.name}`}</CardText>
+              <CardText>{`Số ngày nghỉ còn lại: ${selectedStaff.annualLeave}`}</CardText>
+              <CardText>{`Số ngày đã làm thêm: ${selectedStaff.overTime}`}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  };
 };
