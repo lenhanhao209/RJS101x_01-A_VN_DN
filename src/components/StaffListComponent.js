6cbbdfbef31387041cc2a625bbe9d10a5c8654eb
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-21 10:57:00 +0700

    Declaring StaffList

---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
+import dateFormat from "dateformat";
+
+const StaffList = (props) => {
+  const [selectedStaff, setSelectedStaff] = useState(null);
+  const [columDefault, setColumDefault] = useState(
+    "col-12 col-md-6 col-lg-4 mt-3"
+  );
+};
