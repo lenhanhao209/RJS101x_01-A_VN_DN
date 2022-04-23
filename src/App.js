01a53051bb557330ec80752e9e34fc754bd4b4a6
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-23 10:38:54 +0700

    Lab4_1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import StaffList from "./components/StaffListComponent.js";
-import { STAFFS } from "./shared/constants";
-import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
 
-//Declaring App
-const App = () => {
-  const [staffs, setStaffs] = useState(STAFFS);
-  return (
-    //Display Navbar and staffs when click
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-        </div>
-      </Navbar>
-      <StaffList staffs={staffs} />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Main />
+      </div>
+    );
+  }
+}
 
 export default App;
