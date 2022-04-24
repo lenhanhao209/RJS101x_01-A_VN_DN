f0e68f653adf576a6d27f968f10033f592ae01a9
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-24 10:09:06 +0700

    Lab 4_2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import Main from "./components/MainComponent";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Main />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
 
 export default App;
