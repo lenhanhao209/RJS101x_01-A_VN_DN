f6b33abb4b08b75930fb7aeec5be0d99a73b256e
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-24 22:28:53 +0700

    Lab2-2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-class App extends Component {
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Navbar, NavBarBand } from "reactstrap";
+import "./App.css";
 
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavBarBand href="/">Ristorante Con Fusion</NavBarBand>
+          </div>
+        </Navbar>
       </div>
     );
   }
