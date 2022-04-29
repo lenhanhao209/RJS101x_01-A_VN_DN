9e9c4152c930caf1ccb4924b11508d5709d4af06
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-29 23:11:59 +0700

    The First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
+import Main from "./components/Main";
 import { Navbar, NavbarBrand } from "reactstrap";
-import StaffList from "./components/StaffListComponent.js";
 import { STAFFS } from "./shared/constants";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
-//Declaring App
 const App = () => {
-  const [staffs, setStaffs] = useState(STAFFS);
   return (
-    //Display Navbar and staffs when click
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-        </div>
-      </Navbar>
-      <StaffList staffs={staffs} />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Main.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import StaffList from "./StaffList";
+import StaffDetail from "./StaffDetail";
+import Department from "./Department";
+import Salary from "./Salary";
+import { Route, Routes } from "react-router-dom";
+import { STAFFS, DEPARTMENTS } from "../shared/constants";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Main = (props) => {
+  const [staffs, setStaffs] = useState(STAFFS);
+  const [departments, setDepartments] = useState(DEPARTMENTS);
+
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route
+          exact
+          path="/nhanvien"
+          element={
+            <StaffList
+              staffs={staffs}
+              onStaffSelect={<StaffDetail staffs={staffs} />}
+            />
+          }
+        />
+        <Route path="/nhanvien/:id" element={<StaffDetail staffs={staffs} />} />
+        <Route
+          path="/phongban"
+          element={<Department departments={departments} />}
+        />
+        <Route path="/luong" element={<Salary salarys={staffs} />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
