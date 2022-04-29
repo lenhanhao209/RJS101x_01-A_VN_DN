b69621a72bc23d1d74deb07488beecb6eb901780
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-04-29 23:20:46 +0700

    The fiveth commit - Display the department

---
+++ src/components/Department.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardText,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+  CardBody,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+const Department = (props) => {
+  const department = props.departments.map((depart) => {
+    return (
+      <Card key={depart.id} className="col-lg-3 col-md-5 col-sm-12 mb-3 m-2">
+        <CardTitle>{depart.name}</CardTitle>
+        <CardBody>
+          <CardText> Số lượng nhân viên: {depart.numberOfStaff}</CardText>
+        </CardBody>
+      </Card>
+    );
+  });
+  return (
+    <div className="container">
+      <div className="row">{department}</div>
+    </div>
+  );
+};
+
+export default Department;
