ffcc771e640ef929043999f821911d334ed5fd6d
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-05-02 07:35:16 +0700

    Lab6-3

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,14 +7,49 @@ import {
   Collapse,
   NavItem,
   Jumbotron,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Label,
+  Input,
+  Form,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
 function Header() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [state, setState] = useState({
+    isNavOpen: false,
+    isModalOpen: false,
+  });
+
+  // const [isNavOpen, setIsNavOpen] = useState(false);
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
+    setState({
+      isNavOpen: !state.isNavOpen,
+    });
+  };
+
+  const toggleModal = () => {
+    setState({
+      isModalOpen: !state.isModalOpen,
+    });
+  };
+
+  const handleLogin = (event) => {
+    toggleModal();
+    alert(
+      "Username: " +
+        state.username.value +
+        " Password: " +
+        state.password.value +
+        " Remember: " +
+        state.remember.checked
+    );
+    event.preventDefault();
   };
 
   return (
@@ -30,7 +65,7 @@ function Header() {
               alt="Ristorante Con Fusion"
             />
           </NavbarBrand>
-          <Collapse isOpen={isNavOpen} navbar>
+          <Collapse isOpen={state.isNavOpen} navbar>
             <Nav navbar>
               <NavItem>
                 <NavLink className="nav-link" to="/home">
@@ -53,6 +88,13 @@ function Header() {
                 </NavLink>
               </NavItem>
             </Nav>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Button outline onClick={toggleModal}>
+                  <span className="fa fa-sign-in fa-lg"></span>Login
+                </Button>
+              </NavItem>
+            </Nav>
           </Collapse>
         </div>
       </Navbar>
@@ -70,6 +112,44 @@ function Header() {
           </div>
         </div>
       </Jumbotron>
+      <Modal isOpen={state.isModalOpen} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Login</ModalHeader>
+        <ModalBody>
+          <Form onSubmit={handleLogin}>
+            <FormGroup>
+              <Label htmlFor="username">Username</Label>
+              <Input
+                type="text"
+                id="username"
+                name="username"
+                innerRef={(input) => (state.username = input)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="password">Password</Label>
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                innerRef={(input) => (state.password = input)}
+              />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input
+                  type="checkbox"
+                  name="remember"
+                  innerRef={(input) => (state.remember = input)}
+                />
+                Remember me
+              </Label>
+            </FormGroup>
+            <Button type="submit" value="submit" className="bg-primary">
+              Login
+            </Button>
+          </Form>
+        </ModalBody>
+      </Modal>
     </React.Fragment>
   );
 }
