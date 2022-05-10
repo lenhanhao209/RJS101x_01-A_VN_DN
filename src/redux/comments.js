5523b84c4ba0a3e2b6cae3323cfcd098fc79a8a1
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-05-10 10:32:11 +0700

    Lab8-2

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -4,9 +4,9 @@ import { COMMENTS } from "../shared/comments";
 export const Comments = (state = COMMENTS, action) => {
   switch (action.type) {
     case ActionTypes.ADD_COMMENT:
-      var comment = action.payolad;
+      var comment = action.payload;
       comment.id = state.length;
-      comment.date = new Date.toISOString();
+      comment.date = new Date().toISOString();
       return state.concat(comment);
     default:
       return state;
