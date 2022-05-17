35b8916168370e385939b42c5b63e81c78170f66
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-05-17 11:14:01 +0700

    Lab11-1

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -21,16 +21,6 @@ const About = (props) => {
             <p>{leader.description}</p>
           </div>
         </div>
-        {/* <Media tag="li">
-          <Media left middle>
-            <Media object src={leader.image} alt={leader.name} />
-          </Media>
-          <Media body className="ml-5">
-            <Media heading>{leader.name}</Media>
-            <p>{leader.designation}</p>
-            <p>{leader.description}</p>
-          </Media>
-        </Media> */}
       </div>
     );
   });
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,8 +9,9 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { propTypes } from "redux-form";
 
 const Main = () => {
   const dishes = useSelector((state) => state.dishes);
@@ -20,17 +21,25 @@ const Main = () => {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route exact path="/menu" element={<Menu />} />
-        <Route
-          exact
-          path="/menu/:id"
-          element={<DishDetail dishes={dishes} comments={comments} />}
-        />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/about" element={<About leaders={leaders} />} />
-      </Routes>
+      <TransitionGroup>
+        <CSSTransition
+          // key={this.props.locations.key}
+          classNames="page"
+          timeout={300}
+        >
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route exact path="/menu" element={<Menu />} />
+            <Route
+              exact
+              path="/menu/:id"
+              element={<DishDetail dishes={dishes} comments={comments} />}
+            />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/about" element={<About leaders={leaders} />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
       <Footer />
     </div>
   );
